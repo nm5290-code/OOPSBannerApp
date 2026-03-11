77de7c290b66f3b5f72447da69483b17848b60c7
nm5290-code <nm5290@srmist.edu.in>
2026-03-11 10:49:26 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,3 @@
-/**
- * OOPSBannerApp UC7 - Store Character Pattern in a Class
- *
- * This use case extends UC6 by implementing a CharacterPatternMap class to encapsulate
- * character-to-pattern mappings. The application retrieves and displays the "OOPS"
- * banner using these mappings. This approach enhances code organization and modularity.
- *
- * @author Nimrat
- * @version 7.0
- */
-
-// Extend the User Story 6 to implement a CharacterPatternMap class to encapsulate
-// character-to-pattern mappings. The application retrieves and displays the "OOPS"
-// banner using these mappings. Thereby addressing the drawback of not having
-// centralized character pattern management system.
-
-// Key Requirements:
-// 1. Create CharacterPatternMap class to hold character and its pattern
-// 2. Implement methods to create and retrieve character patterns
-// 3. Use CharacterPatternMap to display the "OOPS" banner
-// 4. Implement modular and reusable character pattern management
-// Drawback of this approach is that we are creating CharacterPatternMap objects ...
-
 public class OOPSBannerApp {
 
     /**
