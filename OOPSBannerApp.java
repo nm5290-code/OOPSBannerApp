017e7fd7f3643fd4af5371b6401a92968298dab0
Nimrat <nm5290@srmist.edu.in>
2026-03-11 09:15:28 +0530

    UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,18 +7,23 @@
  * @author Nimrat
  * @version 3.0
  */
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
+        // Define a String array
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ", " ***** ", " ***** ", " ******", " ******"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     "));
-        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     "));
-        System.out.println(String.join(" ", "*     *", "*     *", "****** ", " *****"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "     *"));
-        System.out.println(String.join(" ", "*     *", "*     *", "*      ", "*    *"));
-        System.out.println(String.join(" ", " ***** ", " ***** ", "*      ", " ****"));
+        lines[0] = String.join(" ", " ***** ", " ***** ", " ******", " ******");
+        lines[1] = String.join(" ", "*     *", "*     *", "*     *", "*     ");
+        lines[2] = String.join(" ", "*     *", "*     *", "*     *", "*     ");
+        lines[3] = String.join(" ", "*     *", "*     *", "****** ", " *****");
+        lines[4] = String.join(" ", "*     *", "*     *", "*      ", "     *");
+        lines[5] = String.join(" ", "*     *", "*     *", "*      ", "*    *");
+        lines[6] = String.join(" ", " ***** ", " ***** ", "*      ", " ****");
+        
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
 
     }
 }
