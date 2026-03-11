628872db0511f113b23559b308213ab19e2a4719
Nimrat <nm5290@srmist.edu.in>
2026-03-11 09:20:31 +0530

    UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,24 @@
 /**
- * OOPSBannerApp UC3 - OOPS Banner using String.join()
+ * OOPSBannerApp UC5 - OOPS Banner using String.join()
  *
  * This program prints OOPS banner using String.join()
  * for better memory efficiency and cleaner code.
  *
  * @author Nimrat
- * @version 3.0
+ * @version 5.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Define a String array
-        String[] lines = new String[7];
-
-        lines[0] = String.join(" ", " ***** ", " ***** ", " ******", " ******");
-        lines[1] = String.join(" ", "*     *", "*     *", "*     *", "*     ");
-        lines[2] = String.join(" ", "*     *", "*     *", "*     *", "*     ");
-        lines[3] = String.join(" ", "*     *", "*     *", "****** ", " *****");
-        lines[4] = String.join(" ", "*     *", "*     *", "*      ", "     *");
-        lines[5] = String.join(" ", "*     *", "*     *", "*      ", "*    *");
-        lines[6] = String.join(" ", " ***** ", " ***** ", "*      ", " ****");
-        
+        // Declare and initialize a String array with inline String.join() calls
+        String[] lines = {
+            String.join(" ", " ***** ", " ***** ", " ******", " ******"),
+            String.join(" ", "*     *", "*     *", "*     *", "*     "),
+            String.join(" ", "*     *", "*     *", "*     *", "*     "),
+            String.join(" ", "*     *", "*     *", "****** ", " *****"),
+            String.join(" ", "*     *", "*     *", "*      ", "     *"),
+            String.join(" ", "*     *", "*     *", "*      ", "*    *"),
+            String.join(" ", " ***** ", " ***** ", "*      ", " ****")
+        };
 
         for (String line : lines) {
             System.out.println(line);
