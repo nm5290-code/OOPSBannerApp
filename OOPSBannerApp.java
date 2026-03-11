76814d0a82205479fdcb6ab265ebd3d81f3a665c
nm5290-code <nm5290@srmist.edu.in>
2026-03-11 10:47:18 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,3 @@
-/**
- * OOPSBannerApp UC3 - OOPS Banner using String.join()
- *
- * This program prints OOPS banner using String.join()
- * for better memory efficiency and cleaner code.
- *
- * @author Nimrat
- * @version 3.0
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // Define a String array
